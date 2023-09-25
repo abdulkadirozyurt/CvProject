7f2ce428b004bdb47fb5b8551bed06f4463d9c9f
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-09-25 15:59:28 +0300

    Repository Design Pattern

---
+++ CvProject/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using CvProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CvProject.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        CvDatabaseEntities entities = new CvDatabaseEntities();
+
+        public List<T> List()
+        {
+            return entities.Set<T>().ToList();
+        }
+
+        public void Add(T t)
+        {
+            entities.Set<T>().Add(t);
+            entities.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            entities.Set<T>().Remove(t);
+            entities.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return entities.Set<T>().Find(id);
+
+        }
+
+        public void Update(T t)
+        {
+            entities.SaveChanges();
+        }
+    }
+}
