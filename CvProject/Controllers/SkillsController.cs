c09653923a359d29b186c407733ad47116802aa7
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2023-09-26 15:34:40 +0300

    Skill Operations completed

--- CvProject/Controllers/SkillsController.cs
+++ CvProject/Controllers/SkillsController.cs
@@ -20,5 +20,44 @@ namespace CvProject.Controllers
 
             return View(skills);
         }
+
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(Skill skill)
+        {
+            repository.Add(skill);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var skill = repository.Find(x => x.Id == id);
+            repository.Delete(skill);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var skill = repository.Find(x => x.Id == id);
+            return View(skill);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSkill(Skill skill)
+        {
+            var skillToUpdate = repository.Find(x=> x.Id == skill.Id);
+            skillToUpdate.SkillName = skill.SkillName;
+            skillToUpdate.SkillRatio = skill.SkillRatio;
+            repository.Update(skill);
+
+            return RedirectToAction("Index");
+        }
     }
 }
